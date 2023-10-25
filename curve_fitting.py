7ff470d5e31d33f06618580dfe44e02ada458c75
Pronton2001 <trihuynhtk15@gmail.com>
2023-10-25 20:03:43 +0700

    update

--- curve_fitting.py
+++ curve_fitting.py
@@ -1,9 +1,12 @@
 import numpy as np
 import cv2
 
-X = np.load('X_out.npy')
-y = np.load('y_out.npy').reshape(-1,1)
-print(X.shape, y.shape)
+X,y = np.load('outer.npy')
+
+# outer = np.load('outer.npy')
+# X = np.load('X_out.npy')
+# y = np.load('y_out.npy').reshape(-1,1)
+# print(X.shape, y.shape)
 # with open('X.npy', 'wb') as f:
 #     np.save(f, X)
 # with open('y.npy', 'wb') as f:
@@ -12,10 +15,13 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures, SplineTransformer
 from sklearn.linear_model import RANSACRegressor, HuberRegressor
 
-# estimator = HuberRegressor()
-estimator = RANSACRegressor(random_state=0)
+h, w = 723,383
+viz_edge = np.zeros((h, w, 3))
+
+estimator = HuberRegressor()
+# estimator = RANSACRegressor(random_state=0)
 # model = make_pipeline(PolynomialFeatures(2), estimator)
-model = make_pipeline(SplineTransformer(n_knots=5, degree=1), estimator)
+model = make_pipeline(SplineTransformer(n_knots=6, degree=3), estimator)
 # print(cv2.UMat(np.array(list(zip(X,y)), dtype=np.uint8).reshape(-1,1,2)))
 points = cv2.UMat(np.array(list(zip(X,y))).reshape(-1,1,2))
 print(points.get().shape)
@@ -23,17 +29,19 @@ print(type(points.get().shape))
 ellipse = cv2.fitEllipse(points)
 
 model.fit(X, y)
-y_pred = model.predict(X)
+X_test = np.linspace(min(X), max(X), 100)
+y_pred = model.predict(X_test)
 
-viz_edge = np.zeros((788, 908, 3))
         # cv2.line(viz_edge, s[:2], s[2:],color=(125,125,125), thickness=2)
 # viz_edge = np.zeros(img.shape)
 cv2.imshow(' edge', viz_edge)
-for i,j,z in zip(X,y_pred,y):
+for i,j in zip(X,y):
     print(i,j)
-    cv2.circle(viz_edge, (int(i),int(j)), color=(0,125,125), radius=2)
-    cv2.circle(viz_edge, (int(i),int(z)), color=(125,125,125), radius=2)
-cv2.ellipse(viz_edge, ellipse, (0, 255, 0), 2)
+    cv2.circle(viz_edge, (int(i),int(j)), color=(0,125,0), radius=2)
+for i,j in zip(X_test,y_pred):
+    print(i,j)
+    cv2.circle(viz_edge, (int(i),int(j)), color=(0,125,125), radius=1)
+# cv2.ellipse(viz_edge, ellipse, (0, 255, 0), 2)
 cv2.imshow('predicted edge', viz_edge)
 
-cv2.waitKey(5000)
+cv2.waitKey(0)
--- edge_searching.py
+++ edge_searching.py
@@ -34,21 +34,34 @@ def slide(new_p, x1, y1, x2, y2, dx = 8, dy = 8):
     x2, y2 = x1 + W_SIZE, y1 - W_SIZE
     return x1, y1, x2, y2
 
-def dist1(p1,p2,p3):
+def dist3point(p1,p2,p3):
     return np.cross(p2-p1,p3-p1)/np.linalg.norm(p2-p1)
+
+def dist2point(p1,p2):
+    return np.linalg.norm(p1 - p2)
 
 def medianPoint(img):
     w,h = img.shape[:2]
     l = []
     for x in range(w):
         for y in range(h):
-            if img[y][x]:
-                l.append([x,y])
+            try:
+                if img[y][x]:
+                    l.append([x,y])
+            except:
+                pass
     return np.median(l, axis = 0)
 
+def save_for_train(segs, title='inner'):
+    segs = np.array(segs)
+    X = np.concatenate((segs[:, 0], segs[:, 2]) , axis = 0).reshape(-1, 1)
+    y = np.concatenate((segs[:, 1], segs[:, 3]) , axis = 0).reshape(-1, 1)
+    
+    with open(f'{title}.npy', 'wb') as f:
+        np.save(f, (X,y))
 if __name__ == '__main__':
 
-    img = cv2.imread('/home/pronton/SeAH/data/cam4_20230907_143145/120.jpg') # img = cv2.imread(argv[1])
+    img = cv2.imread('/home/pronton/SeAH/data/cam4_20230907_143145/250.jpg') # img = cv2.imread(argv[1])
 
     ''' 1. Preprocessing '''
     img = cv2.bilateralFilter(img, 9, 75, 75)
@@ -57,7 +70,7 @@ if __name__ == '__main__':
     h_img, w_img = img.shape[:2]
 
     ''' canny '''
-    canny_img = cv2.Canny(img, 50, 300)
+    canny_img = cv2.Canny(img, 50, 200)
     cv2.imshow('canny', canny_img)
 
     ''' hsv_mask '''
@@ -68,12 +81,13 @@ if __name__ == '__main__':
 
     ''' Initialize window starting point '''
     W_SIZE = 35
-    startX, startY = .193 , .93
+    startX, startY = .193 , .93 #TODO - not fixed but auto detect starting point
     x1, y1 = int(startX*w_img -W_SIZE/2), int(startY * h_img + W_SIZE/2)
     x2, y2 = x1+W_SIZE, y1-W_SIZE
     prev_angle = 15
     inner_segs = []
     outer_segs = []
+    centerP = (w_img//4, h_img//2)
 
     ''' Slide window '''
     for _ in range(100):
@@ -101,7 +115,7 @@ if __name__ == '__main__':
         if len(segments) >=2:
             p = segments[0]
             other = segments[1:]
-            dist2p = np.array([np.abs(dist1(p[:2], p[2:], (op[:2]+op[2:])/2)) for op in other]) # absolute distance from p to other segs
+            dist2p = np.array([np.abs(dist3point(p[:2], p[2:], (op[:2]+op[2:])/2)) for op in other]) # absolute distance from p to other segs
             diffp_segs = other[dist2p > 4]
             samep_segs = other[dist2p < 2]
             samep_segs = np.concatenate((samep_segs.reshape(-1,4), p.reshape(-1,4)), axis= 0)
@@ -109,21 +123,70 @@ if __name__ == '__main__':
 
             drawSegments(samep_segs, prompt = 'same')
             mean_same = np.mean(samep_segs, axis= 0, dtype=int)
+            center_mean_same_global = (mean_same[:2] + mean_same[2:])//2  + [x1,y2]
+            radius_same = dist2point(centerP, center_mean_same_global)
+            samep_segs_global = samep_segs  + [x1,y2,x1,y2]
+
             if len(diffp_segs):
                 drawSegments(diffp_segs, prompt = 'diff')
                 mean_diff = np.mean(diffp_segs, axis= 0)
                 mean_ = np.mean((mean_same, mean_diff), axis= 0, dtype = int)
                 x1, y1, x2, y2 = slide(mean_[2:], x1, y1,x2,y2)
+
+                center_mean_diff_global = (mean_diff[:2] + mean_diff[2:])//2 + [x1,y2]
+                radius_diff = dist2point(centerP, center_mean_diff_global)
+
+                diffp_segs_global = diffp_segs  + [x1,y2,x1,y2]
+
+                if radius_diff  < radius_same:
+                    inner_segs.extend(diffp_segs_global)
+                    outer_segs.extend(samep_segs_global)
+                else:
+                    inner_segs.extend(samep_segs_global)
+                    outer_segs.extend(diffp_segs_global)
+                
+                # draw inner/outer lines
+                viz_segs = np.zeros(img.shape)
+                print('same_global', samep_segs_global)
+                for i in inner_segs:
+                    cv2.circle(viz_segs, i[:2], color = (0,0, 255), radius = 1 )
+                    cv2.circle(viz_segs, i[2:], color = (0,0, 255), radius = 1 )
+                for i in outer_segs:
+                    cv2.circle(viz_segs, i[:2], color = (255,0,0), radius = 1 )
+                    cv2.circle(viz_segs, i[2:], color = (255,0,0), radius = 1 )
+                cv2.imshow('segs', viz_segs)
+                
             else:
                 x1, y1, x2, y2 = slide(mean_same[2:], x1, y1,x2,y2)
+                if len(inner_segs) and len(outer_segs):
+                    dist2inner = dist2point(mean_same[:2], inner_segs[-1][2:])
+                    dist2outer = dist2point(mean_same[:2], outer_segs[-1][2:])
+                    if dist2inner < dist2outer:
+                        inner_segs.extend(samep_segs_global)
+                    else:
+                        outer_segs.extend(samep_segs_global)
+                else: # outer seg is more likely detected by Canny than inner seg
+                    outer_segs.extend(samep_segs_global)
+                    
             # mean_same = np.mean()
             # x1, y1, x2, y2 = slide(segments[0][2:], x1, y1,x2,y2)
             
         elif len(segments) == 1:
             p = segments[0]
-            print('prev loc', x1, y1, x2, y2  )
+            p_global = np.array(p).reshape(-1,4) + [x1, y2, x1, y2]
+            print('p_global', p_global)
             x1, y1, x2, y2 = slide(segments[0][2:], x1, y1,x2,y2)
-            print('new loc', x1, y1, x2, y2  )
+
+            if len(inner_segs) and len(outer_segs):
+                dist2inner = dist2point(p[:2], inner_segs[-1][2:])
+                dist2outer = dist2point(p[:2], outer_segs[-1][2:])
+                if dist2inner < dist2outer:
+                    inner_segs.extend(p_global)
+                else:
+                    outer_segs.extend(p_global)
+            else: # outer seg is more likely detected by Canny than inner seg
+                outer_segs.extend(p_global)
+            
         else: # move base on previous inner, outer angle?
             targetP = medianPoint(w_mask).astype(int)
             cv2.circle(w_mask, targetP, color = (125,125,125), radius = 2)
@@ -131,3 +194,7 @@ if __name__ == '__main__':
             x1, y1, x2, y2 = slide(targetP, x1, y1, x2, y2)
             
         cv2.waitKey(0)
+    
+    # save
+    save_for_train(inner_segs, 'inner')
+    save_for_train(outer_segs, 'outer')
